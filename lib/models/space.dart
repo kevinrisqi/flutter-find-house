985850baf077a81732dbcdadc605bd887dda910f
kevinrisqi <kevinrisqi22@gmail.com>
2022-03-24 21:01:47 +0700

    Modify space class

--- lib/models/space.dart
+++ lib/models/space.dart
@@ -2,10 +2,17 @@ class Space {
   int id;
   String name;
   int price;
-  double rating;
+  int rating;
   String city;
   String country;
   String imageUrl;
+  String address;
+  String phone;
+  String mapUrl;
+  List photos;
+  int numberOfKitchens;
+  int numberOfBedrooms;
+  int numberOfCupboards;
 
   Space({
     required this.id,
@@ -15,5 +22,30 @@ class Space {
     required this.city,
     required this.country,
     required this.imageUrl,
+    required this.address,
+    required this.phone,
+    required this.mapUrl,
+    required this.photos,
+    required this.numberOfKitchens,
+    required this.numberOfBedrooms,
+    required this.numberOfCupboards
   });
+
+  Space.fromJson(json) {
+    id = json['id'];
+    name = json['name'];
+    city = json['city'];
+    country = json['country'];
+    price = json['price'];
+    imageUrl = json['image_url'];
+    rating = json['rating'];
+    address = json['address'];
+    phone = json['phone'];
+    mapUrl = json['map_url'];
+    numberOfKitchens = json['number_of_kitchens'];
+    numberOfBedrooms = json['number_of_bedrooms'];
+    numberOfCupboards = json['number_of_cupboards'];
+  }
+
+  
 }
